31395c25e9257f32dc4faa0a3dc88cbdd308f81a
jcamiloif6 <46933022+jcamiloif6@users.noreply.github.com>
2024-10-20 14:09:03 -0500

    Update app2.py

--- Data Analist Challenge/Reto 1 - Migracion de base de Datos/Inserting through API/app2.py
+++ Data Analist Challenge/Reto 1 - Migracion de base de Datos/Inserting through API/app2.py
@@ -12,7 +12,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Clave-123",
+        password="clave",
         database="challenge"
     )
 
